f4e8160f5bb98e92aede0ac9c70a295d1dee3c97
naga6677 <86395145+naga6677@users.noreply.github.com>
2022-02-22 16:30:20 +0530

    Spring Cloud Config Server (#4)
    * Hello world Controller
    
    * Initial commit of Microservices Projects
    
    * deatils
    
    * Updated Details
    
    * Updated Docker hub details
    
    * Spring Cloud Config Server Implementation.

--- cards/src/main/java/com/microservices/learning/cards/controller/CardsController.java
+++ cards/src/main/java/com/microservices/learning/cards/controller/CardsController.java
@@ -3,15 +3,31 @@ package com.microservices.learning.cards.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microservices.learning.cards.config.CardServiceConfig;
 
 import com.microservices.learning.cards.entity.Cards;
 import com.microservices.learning.cards.entity.Customer;
 
 @RestController
 public class CardsController {
+
+	
+	@Autowired
+	private CardServiceConfig cardServiceConfig;
+
 
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -27,5 +43,21 @@ public class CardsController {
 
 		return tCards;
 	}
+	
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties();
+
+		properties.setProperty("Msg", cardServiceConfig.getMsg());
+		properties.setProperty("Build Version", cardServiceConfig.getBuildVersion());
+
+		properties.put("MailDetails", cardServiceConfig.getMailDetails());
+		properties.put("Active Branches", cardServiceConfig.getActiveBranches());
+
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+
 
 }
--- loans/src/main/java/com/microservices/learning/loans/controller/LoanController.java
+++ loans/src/main/java/com/microservices/learning/loans/controller/LoanController.java
@@ -3,15 +3,31 @@ package com.microservices.learning.loans.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microservices.learning.loans.config.LoansServiceConfig;
 
 import com.microservices.learning.loans.entity.Customer;
 import com.microservices.learning.loans.entity.Loans;
 
 @RestController
 public class LoanController {
+
+
+	@Autowired
+	private LoansServiceConfig loansServiceConfig;
+
 
 	@PostMapping("/myLoans")
 	public List<Loans> getLoanDetails(@RequestBody Customer customer) {
@@ -27,5 +43,21 @@ public class LoanController {
 
 		return tLoans;
 	}
+
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties();
+
+		properties.setProperty("Msg", loansServiceConfig.getMsg());
+		properties.setProperty("Build Version", loansServiceConfig.getBuildVersion());
+
+		properties.put("MailDetails", loansServiceConfig.getMailDetails());
+		properties.put("Active Branches", loansServiceConfig.getActiveBranches());
+
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+
 
 }
