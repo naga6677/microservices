25a3ce54037439d09a4bd194222aeea4043c6e5d
naga6677 <86395145+naga6677@users.noreply.github.com>
2022-02-24 12:11:57 +0530

    Microservices Intercommunication (#6)
    * Hello world Controller
    
    * Initial commit of Microservices Projects
    
    * deatils
    
    * Updated Details
    
    * Updated Docker hub details
    
    * Spring Cloud Config Server Implementation.
    
    * Eurekaserver Initial Commit. Service Discovery and Config server chnages.
    
    * Microservices Intercommunication.

--- accounts/src/main/java/com/microservices/learning/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/learning/accounts/controller/AccountsController.java
@@ -1,5 +1,8 @@
 package com.microservices.learning.accounts.controller;
 
+
+import java.util.List;
+
 import java.util.Properties;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.learning.accounts.config.AccountsServiceConfig;
 
 import com.microservices.learning.accounts.entity.Accounts;
+import com.microservices.learning.accounts.entity.Cards;
 import com.microservices.learning.accounts.entity.Customer;
+import com.microservices.learning.accounts.entity.CustomerDetails;
+import com.microservices.learning.accounts.entity.Loans;
+import com.microservices.learning.accounts.service.client.CardsFeignClient;
+import com.microservices.learning.accounts.service.client.LoansFeignClient;
 
 @RestController
 public class AccountsController {
@@ -22,6 +30,11 @@ public class AccountsController {
 	@Autowired
 	AccountsServiceConfig accountsConfig;
 
+	@Autowired
+	CardsFeignClient cardsFeignClient;
+
+	@Autowired
+	LoansFeignClient loansFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -32,7 +45,6 @@ public class AccountsController {
 
 		return tAccounts;
 	}
-
 
 	@GetMapping("/account/properties")
 	public String getPropertyDetails() throws JsonProcessingException {
@@ -49,6 +61,22 @@ public class AccountsController {
 		return jsonStr;
 	}
 
+
+	@PostMapping(value = "/myCustomerDeatils")
+	public CustomerDetails getCustomerDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = getAccountDetails(new Customer());
+		List<Cards> tCards = cardsFeignClient.getCardDetails(new Customer());
+		List<Loans> tLoans = loansFeignClient.getLoanDetails(new Customer());
+
+		CustomerDetails tCustomerDetails = new CustomerDetails();
+		tCustomerDetails.setAccounts(accounts);
+		tCustomerDetails.setCards(tCards);
+		tCustomerDetails.setLoans(tLoans);
+
+		return tCustomerDetails;
+
+	}
 
 
 }
